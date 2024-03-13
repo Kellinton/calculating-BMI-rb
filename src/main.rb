f7f34e806acc5fd128e487fccf5fafd4645bf853
Kellinton <kellintonbrasil@gmail.com>
2024-03-13 07:22:56 -0300

    init

---
+++ src/main.rb
@@ -0,0 +1,14 @@
+# Formula IMC:
+# IMC = peso / (altura * altura)
+
+# Elaborar um algoritmo que dado o peso e a altura de um adulto,
+# mostre sua condição de acordo com a tabela abaixo.
+
+# IMC Condição:
+
+# - Abaixo de 18.5 (Abaixo do Peso)
+# - Entre de 18.5 e 25 (Peso Normal)
+# - Entre 25 e 30 (Acima do Peso)
+# - Entre 30 e 40 (Obeso)
+# - Acima de 40 (Obesidade Grave)
+
