f84ad4ee8f0fe9bb7d3bc7ece5edc9e705a415c7
Kellinton <kellintonbrasil@gmail.com>
2024-03-13 07:27:14 -0300

    calcular imc

--- src/main.rb
+++ src/main.rb
@@ -12,3 +12,28 @@
 # - Entre 30 e 40 (Obeso)
 # - Acima de 40 (Obesidade Grave)
 
+peso = 120
+altura = 1.80
+
+def calcular_imc(peso, altura)
+    imc = peso / (altura ** 2)
+
+    if imc < 18.5
+        puts "IMC: #{'%.2f' % imc}"
+        puts "Abaixo do peso!"
+    elsif imc >= 18.5 && imc <= 25
+        puts "IMC: #{'%.2f' % imc}"
+        puts "Peso normal!"
+    elsif imc >= 25 && imc <= 30
+        puts "IMC: #{'%.2f' % imc}"
+        puts "Acima do peso!"
+    elsif imc >= 30 && imc <= 40
+        puts "IMC: #{'%.2f' % imc}"
+        puts "Obeso!"
+    else
+        puts "IMC: #{'%.2f' % imc}"
+        puts "Obesidade grave!"
+    end
+end
+
+calcular_imc(peso, altura)
